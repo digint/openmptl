bc71e7d1de0fc38b1d3b487c00656e74f4f5b498
Axel Burri <axel@tty0.ch>
2013-01-16 18:16:57 +0100

    Fixed Makefiles, since cppcore is now header-only

--- config/cppcore.mk
+++ config/cppcore.mk
@@ -1,3 +1,15 @@
+#
+# Input Variables:
+#
+# - CPPCORE_TOP  : CppCore top directory
+# - CPPCORE_ARCH : Architecture (e.g. "stm32f10x")
+#
+#
+# Output Variables:
+#
+# - CPPCORE_INCLUDE : compiler include flags
+#
+
 CPPCORE_DOC_DIR     = $(CPPCORE_TOP)/doc
 CPPCORE_ARCH_DIR    = $(CPPCORE_TOP)/arch
 CPPCORE_DRIVERS_DIR = $(CPPCORE_TOP)/drivers
