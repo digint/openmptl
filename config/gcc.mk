a7ff45572651ab300bc67c4983cb86955d3af1b2
Axel Burri <axel@tty0.ch>
2013-07-17 04:20:45 +0200

    clang: added makefile switch CLANG (only for stm32f4discovery)

--- config/gcc.mk
+++ config/gcc.mk
@@ -1,3 +1,15 @@
+ifdef CLANG
+
+CC           = clang
+CXX          = clang++
+AS           = $(CROSS)gcc -x assembler-with-cpp
+LD           = clang++
+OBJCOPY      = $(CROSS)objcopy
+OBJDUMP      = $(CROSS)objdump
+SIZE         = $(CROSS)size -B -d
+
+else
+
 CC           = $(CROSS)gcc
 CXX          = $(CROSS)g++
 AS           = $(CROSS)gcc -x assembler-with-cpp
@@ -5,3 +17,5 @@ LD           = $(CROSS)g++
 OBJCOPY      = $(CROSS)objcopy
 OBJDUMP      = $(CROSS)objdump
 SIZE         = $(CROSS)size -B -d
+
+endif
