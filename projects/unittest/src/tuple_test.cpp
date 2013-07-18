63e784c43d78af5bfd1679b9d9352d1813f95a09
Axel Burri <axel@tty0.ch>
2013-07-18 13:49:34 +0200

    added unittest/src/tuple_test.cpp

---
+++ projects/unittest/src/tuple_test.cpp
@@ -0,0 +1,78 @@
+/*
+ * OpenMPTL - C++ Microprocessor Template Library
+ *
+ * Copyright 2013 Axel Burri <axel@tty0.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <typelist.hpp>
+#include <arch/rcc.hpp>
+
+#include <iostream>
+#include <tuple>
+#include <string>
+
+using namespace mptl;
+using namespace mptl::reg;
+
+void reg_reaction::react() { }
+
+#if 0
+
+// helper function to print a tuple of any size
+template<class Tp, std::size_t N>
+struct tuple_regmask_reset_to {
+  static void command(const Tp& t) 
+  {
+    tuple_regmask_reset_to<Tp, N-1>::command(t);
+    // std::cout << "# " << std::get<N-1>(t).value << std::endl;
+    std::get<N-1>(t).reset_to();
+  }
+};
+ 
+template<class Tp>
+struct tuple_regmask_reset_to<Tp, 1> {
+  static void command(const Tp& t) 
+  {
+    // std::cout << std::get<0>(t).value << std::endl;
+    std::get<0>(t).reset_to();
+  }
+};
+
+template<class... Args>
+void reset_all(const std::tuple<Args...>& t) 
+{
+  std::cout << "****** reset_all ******" << std::endl;
+  tuple_regmask_reset_to<decltype(t), sizeof...(Args)>::command(t);
+  std::cout << "****** end reset_all ******" << std::endl;
+}
+#endif
+ 
+int main()
+{
+  using list = typelist<
+    RCC::PLLCFGR::PLLSRC,
+    RCC::CFGR::PPRE1::DIV4
+    >;
+
+#if 0
+  auto t1 = list::tuple_type();
+  reset_all(t1);
+#else
+  //  auto t1 = list::tuple_type();
+  reset_all(list::tuple_type());
+#endif
+}
