ef220fd05f5d7ac1e0443458dadf1cab11b750da
Axel Burri <axel@tty0.ch>
2013-07-30 20:02:44 +0200

    stm32f4discovery-ledtest: added new project (from llvm branch, modified). clang compilation for target works (with some workaraounds)

--- config/clang.mk
+++ config/clang.mk
@@ -11,30 +11,44 @@ ifndef SIMULATION
 # you might want to uncomment the following:
 #
 
-#   GCC_VERSION=4.8.1
-#   TRIPLE=armv7m-none-eabi
-#
-#   CLANG_SYSROOT    = -isysroot /opt/toolchain/$(TRIPLE)-$(GCC_VERSION)/$(TRIPLE)/
-#   CLANG_SYSROOT   += -iwithsysroot /include/c++/$(GCC_VERSION)
-#   CLANG_SYSROOT   += -iwithsysroot /include/c++/$(GCC_VERSION)/$(TRIPLE)
-#   CLANG_SYSROOT   += -iwithsysroot /include/c++/$(GCC_VERSION)/backward
-#   CLANG_SYSROOT   += -iwithsysroot /include
+GCC_VERSION=4.8.1
+TRIPLE=armv7m-none-eabi
 
-  # use prefixed clang
+TOOLCHAIN_DIR=/opt/toolchain/$(TRIPLE)-$(GCC_VERSION)
+
+ifdef CLANG_CROSS_BUILD    # use prefixed clang
   CROSS_CLANG = $(CROSS)
+else
+  CLANG_SYSROOT += -resource-dir $(TOOLCHAIN_DIR)
+  CLANG_SYSROOT += -isysroot $(TOOLCHAIN_DIR)/$(TRIPLE)
+  CLANG_SYSROOT += -isystem $(TOOLCHAIN_DIR)/$(TRIPLE)/include/c++/$(GCC_VERSION)
+  CLANG_SYSROOT += -isystem $(TOOLCHAIN_DIR)/$(TRIPLE)/include/c++/$(GCC_VERSION)/$(TRIPLE)
+  CLANG_SYSROOT += -isystem $(TOOLCHAIN_DIR)/$(TRIPLE)/include/c++/$(GCC_VERSION)/backward
+  CLANG_SYSROOT += -isystem $(TOOLCHAIN_DIR)/$(TRIPLE)/include
+
+  CLANG_TARGET += -target $(TRIPLE)
+  CLANG_TARGET += -ccc-gcc-name $(TRIPLE)-g++
+  CLANG_TARGET += -gcc-toolchain $(TOOLCHAIN_DIR)
+
+  CROSS_CLANG =
+endif
 
 endif # SIMULATION
 
-CC           = $(CROSS_CLANG)clang $(CLANG_SYSROOT)
-CXX          = $(CROSS_CLANG)clang++ $(CLANG_SYSROOT)
+CC           = $(CROSS_CLANG)clang $(CLANG_SYSROOT) $(CLANG_TARGET)
+CXX          = $(CROSS_CLANG)clang++ $(CLANG_SYSROOT) $(CLANG_TARGET)
 AS           = $(CROSS)gcc -x assembler-with-cpp
-LD           = $(CROSS_CLANG)clang++ $(CLANG_SYSROOT)
+LD           = $(CROSS_CLANG)clang++ $(CLANG_TARGET)
+#LD           = $(CROSS_CLANG)g++ -fuse-ld=gold
+#LD           = $(CROSS)ld.gold --verbose -static -plugin $(TOOLCHAIN_DIR)/lib/LLVMgold.so -L${TOOLCHAIN_DIR}/${TRIPLE}/lib -lc
 OBJCOPY      = $(CROSS)objcopy
 OBJDUMP      = $(CROSS)objdump
 SIZE         = $(CROSS)size -B -d
 
 FLAGS       += -DCONFIG_CLANG
 
+# emit llvm bytecode by default
+CLANG_EMIT_LLVM = 1
 
 ifdef SIMULATION
   # use libcxx (llvm's c++ standard library implementation, yum!)
---
+++ projects/stm32f4discovery-ledtest/src/ledtest.cpp
@@ -0,0 +1,118 @@
+/*
+ * OpenMPTL - C++ Microprocessor Template Library
+ *
+ * Copyright 2013 Axel Burri <axel@tty0.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <arch/core.hpp>
+#include <arch/rcc.hpp>
+#include <arch/pwr.hpp>
+#include <arch/flash.hpp>
+#include <arch/nvic.hpp>
+#include <arch/reg/gpio.hpp>
+#include <typelist.hpp>
+
+static constexpr char     led_port = 'D';
+static constexpr unsigned led_pin  = 12;
+
+using rcc     = mptl::rcc< mptl::mhz(168) >;
+using pwr     = mptl::pwr< rcc, mptl::volt(3.3), false >;
+using flash   = mptl::flash< rcc, pwr >;
+
+#ifdef HIGH_LEVEL
+#include <arch/gpio.hpp>
+using led_green  = mptl::gpio_led< led_port, led_pin >;
+#endif
+
+void __naked reset_isr(void);
+void null_isr(void) { }
+
+using resources = mptl::typelist<
+  mptl::irq_handler< typename mptl::irq::reset, reset_isr >
+#ifdef HIGH_LEVEL
+  , led_green::resources
+#endif
+  >;
+
+
+/* Reset exception: triggered on system startup (system entry point). */
+void __naked reset_isr(void) {
+  mptl::core::startup<rcc, flash, pwr>();
+
+  /* set all register from our resources<> typelist */
+  mptl::core::configure< resources >();
+
+#ifdef HIGH_LEVEL
+  led_green ::on();
+#else
+  static constexpr uint32_t pin_mask = (uint32_t)0x1 << led_pin;
+  mptl::reg::RCC::AHB1ENR::GPIODEN::reset_to();  // enable AHB1 clock for GPIO D
+  mptl::regval< mptl::reg::GPIO< led_port >::MODERx< led_pin >, 1 >::set();  // configure GPIOD::MODERx (set GPIOD12 to output mode)
+  mptl::reg::GPIO< led_port >::BSRR::store(pin_mask);  // enable led
+#endif
+
+  while(1) {
+    mptl::core::nop();
+  }
+}
+
+
+#ifndef OPENMPTL_SIMULATION
+
+#include <arch/vector_table.hpp>
+extern const uint32_t _stack_top;  /* provided by linker script */
+mptl::vector_table<&_stack_top, resources, null_isr > vector_table;
+
+#ifdef CONFIG_CLANG
+// clang-3.3 ignores "__attribute__((used))", use isr_vector[] as a workaround
+mptl::isr_t clang_hack(void) {
+  return vector_table.isr_vector[0];
+}
+#endif // CONFIG_CLANG
+
+#else // OPENMPTL_SIMULATION
+
+#include <iostream>
+
+void mptl::reg_reaction::react() {
+  switch(addr) {
+  case reg::RCC::CR::addr:
+    if(bits_set< reg::RCC::CR::HSEON >()) {
+      reg::RCC::CR::HSERDY::set();
+    }
+    if(bits_set< reg::RCC::CR::PLLON >()) {
+      reg::RCC::CR::PLLRDY::set();
+    }
+    break;
+
+  case reg::RCC::CFGR::addr:
+    if(bits_set< reg::RCC::CFGR::SW::PLL >()) {
+      reg::RCC::CFGR::SWS::PLL::set();
+    }
+    break;
+  };
+};
+
+//int main(int argc, char *argv[])
+int main(void)
+{
+  std::cout << "*** stm32f4discovery demo: starting simulation..." << std::endl;
+
+  reset_isr();
+}
+
+#endif // OPENMPTL_SIMULATION
