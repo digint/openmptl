ca0de04ca5074312d239332af3cd68d87d58a6fd
Axel Burri <axel@tty0.ch>
2013-09-03 13:43:33 +0200

    rcc: generic system_clock_hse setup

--- arch/arm/cortex/stm32/f4/include/arch/rcc.hpp
+++ arch/arm/cortex/stm32/f4/include/arch/rcc.hpp
@@ -69,7 +69,7 @@ public:  /* ------ static member functions ------ */
 
 
 template< typename... Tp >
-struct system_clock_hse_impl
+struct system_clock_hse_base
 {
   using cfg_list = reglist<
     Tp...,
@@ -94,47 +94,100 @@ struct system_clock_hse_impl
   }
 };
 
+
+/**
+ * PLL configuration with fixed PLLP=2
+ */
+template< freq_t output_freq, freq_t hse_freq >
+struct system_clock_pllcfgr {
+  static constexpr freq_t vco_input = mhz(1); // It is recommended to select a frequency of 2 MHz to limit PLL jitter.
+
+  static constexpr unsigned p = 2;
+  static constexpr unsigned m = hse_freq / vco_input;
+  static constexpr unsigned n = (output_freq / vco_input ) * p;
+  static constexpr unsigned q = n / 48 + (n % 48 ? 1 : 0); // use next higher value if n is not divisible without reminder
+
+  static_assert(p==2 || p==4 || p==6 || p==8,  "illegal PLLP value");
+  static_assert(q >= 2   && q <= 15,  "illegal PLLQ value");
+  static_assert(n >= 192 && n <= 432, "illegal PLLN value");
+  static_assert(m >= 2   && m <= 63,  "illegal PLLM value");
+
+  static constexpr freq_t vco_output = vco_input * n;
+  static constexpr freq_t pll_output = vco_output / p;
+  static constexpr freq_t pll48clk   = vco_output / q;
+
+  static_assert(vco_input >= mhz(1) && vco_input <= mhz(2), "illegal VCO input frequency");
+  static_assert(vco_output >= mhz(192) && vco_output <= mhz(432), "illegal VCO output frequency");
+  static_assert(pll_output <= mhz(168), "illegal PLL output frequency");
+
+  /* The USB OTG FS requires a 48 MHz clock to work correctly */
+  static constexpr bool usb_otg_available = ( pll48clk == mhz(48) );
+
+  /* The SDIO and the random number generator need a frequency lower
+   * than or equal to 48 MHz to work correctly. */
+  static constexpr bool sdio_available = ( pll48clk <= mhz(48) );
+  static constexpr bool rng_available  = ( pll48clk <= mhz(48) );
+
+  /* assert on above restrictions for now */
+  static_assert(usb_otg_available, "USB OTG FS is not available");
+  static_assert(sdio_available,    "SDIO is not available");
+  static_assert(rng_available,     "RNG is not available");
+
+  using type = reglist <
+    regval< RCC::PLLCFGR::PLLM, m >,
+    regval< RCC::PLLCFGR::PLLN, n >,
+    regval< RCC::PLLCFGR::PLLP, (p >> 1) - 1 >,
+    regval< RCC::PLLCFGR::PLLQ, q >
+    >;
+
+#ifdef OPENMPTL_SIMULATION
+  static void dump() {
+    std::cout << std::dec << "--------------------" << std::endl;
+    std::cout << "HCLK = " << output_freq / mhz(1) << " MHz" << std::endl;
+    std::cout << "HSE  = " << hse_freq / mhz(1) << " MHz" << std::endl;
+    std::cout << std::dec << "--------------------" << std::endl;
+  
+    std::cout << "m=" << m << std::endl;
+    std::cout << "n=" << n << std::endl;
+    std::cout << "p=" << p << std::endl;
+    std::cout << "q=" << q << std::endl;
+
+    std::cout << "vco_input  = " << (vco_input  / mhz(1)) << " MHz" << std::endl;
+    std::cout << "vco_output = " << (vco_output / mhz(1)) << " MHz" << std::endl;
+    std::cout << "pll_output = " << (pll_output / mhz(1)) << " MHz" << std::endl;
+    std::cout << "pll48clk   = " << (pll48clk   / mhz(1)) << " MHz" << std::endl;
+  }
+#endif
+};
+
+
+/**
+ * Generic HSE system clock setup.
+ *
+ * Provides decent clock setup for
+ * output_freq (HCLK): 168MHz, 144MHz, 120Mhz, 96MHz.
+ *
+ * Template arguments:
+ *
+ *   - output_freq: desired output (HCLK) frequency
+ *   - hse_freq: frequency of the HSE external oscillator
+ */
 template< freq_t output_freq, freq_t hse_freq = mhz(8) >
-struct system_clock_hse {
-  static_assert(output_freq == !output_freq,
-                "unsupported system clock frequency (see available template specialisations below)");
-};
-
-template< freq_t hse_freq >
-struct system_clock_hse< mhz(120), hse_freq >
-: public system_clock_hse_impl <
+struct system_clock_hse
+: public system_clock_hse_base<
   RCC::CFGR::HPRE ::DIV1,
   RCC::CFGR::PPRE1::DIV4,
   RCC::CFGR::PPRE2::DIV2,
-  regval< RCC::PLLCFGR::PLLM, hse_freq / mhz(1) >,
-  regval< RCC::PLLCFGR::PLLN, 240 >,
-  regval< RCC::PLLCFGR::PLLP, 0   >, // DIV2
-  regval< RCC::PLLCFGR::PLLQ, 5   >
+  typename system_clock_pllcfgr< output_freq, hse_freq >::type
   >
 {
-  static constexpr freq_t hclk_freq  = mhz(120);
+  static_assert( output_freq <= mhz(168), "illegal output (HCLK) frequency" );
+  static_assert( hse_freq >= mhz(4) && hse_freq <= mhz(26), "illegal HSE frequency" );
+
+  static constexpr freq_t hclk_freq  = output_freq;
   static constexpr freq_t pclk1_freq = hclk_freq / 4;
   static constexpr freq_t pclk2_freq = hclk_freq / 2;
 };
-
-template< freq_t hse_freq >
-struct system_clock_hse< mhz(168), hse_freq >
-: public system_clock_hse_impl <
-  RCC::CFGR::HPRE ::DIV1,
-  RCC::CFGR::PPRE1::DIV4,
-  RCC::CFGR::PPRE2::DIV2,
-  regval< RCC::PLLCFGR::PLLM, hse_freq / mhz(1) >,
-  regval< RCC::PLLCFGR::PLLN, 336 >,
-  regval< RCC::PLLCFGR::PLLP, 0   >, // DIV2
-  regval< RCC::PLLCFGR::PLLQ, 7   >
-  >
-{
-  static constexpr freq_t hclk_freq  = mhz(168);
-  static constexpr freq_t pclk1_freq = hclk_freq / 4;
-  static constexpr freq_t pclk2_freq = hclk_freq / 2;
-};
-
-
 
 
 ////////////////////  peripheral clock traits  ////////////////////
